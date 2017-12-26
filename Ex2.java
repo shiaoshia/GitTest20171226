0d25974d97fcf5ed699c5a26fe182f70da5ea04a
shiaoshia <34782716+shiaoshia@users.noreply.github.com>
2017-12-26 21:00:21 +0800

    Clone test

--- Ex2.java
+++ Ex2.java
@@ -1,7 +1,7 @@
 class Ex2{
 	public static void main(String[] args){
 		System.out.println("Local Test");
-		System.out.println("Local OK);
-		
+		System.out.println("Local OK");
+		System.out.println("Local Clone");
 	}
 }
