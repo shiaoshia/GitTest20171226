265fd632f96c73177435cd515246e90f8615c467
shiaoshia <34782716+shiaoshia@users.noreply.github.com>
2017-12-26 14:51:52 +0800

    合併

--- Ex1.java
+++ Ex1.java
@@ -1,13 +1,7 @@
 class Ex1{
 	public static void main(String[] args){
 		System.out.println("OK");
-<<<<<<< HEAD
-		System.out.println("Master Test");
-=======
 		System.out.println("快速修正");
-		System.out.println("快速修正ok啦");
->>>>>>> QuickFix
-		System.out.println("合併檔");
-		System.out.println("合併");
+		System.out.println("合併版");
 	}
 }
